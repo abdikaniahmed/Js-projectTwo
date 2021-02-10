67daa938425c43a8bbf3d98840b64da88c231164
Abdikani Ahmed <cabdiqanisoomaal@gmail.com>
2021-02-10 19:59:48 +0300

    Add Localstorage

--- script.js
+++ script.js
@@ -6,26 +6,60 @@ const movieSelect = document.getElementById('Movie');
 
 let ticketPrice = +movieSelect.value;
 
+PopulateLUI();
+ 
+//set Movie data
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('movieIndex', movieIndex);
+    localStorage.setItem('MoviePrice', moviePrice);
+}
+
 // count  
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    const selectedSeatsCount = selectedSeats.length
+    const selectedSeatsCount = selectedSeats.length;
+
+    const seatIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+    
+    localStorage.setItem("selectedSeat", JSON.stringify(seatIndex));
     
     count.innerHTML = selectedSeatsCount;
     total.innerHTML = selectedSeatsCount * ticketPrice;
 }
 
+//Populate Ul 
+function PopulateLUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeat"));
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add("selected")
+            }
+        })
+    }
+
+    const selectedMovie = localStorage.getItem("movieIndex");
+    if (selectedMovie !== null) {
+        movieSelect.selectedIndex = selectedMovie;
+    }
+}
+
 // Change Movies 
 movieSelect.addEventListener('change', e => {
     ticketPrice = e.target.value;
-    updateSelectedCount()
+   setMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
 })
 
 // click Seat Event
-container.addEventListener('click', (e) => {
+container.addEventListener('click', e => {
     
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle('selected');
         updateSelectedCount();
     }
 })
+
+// initial count total
+
+updateSelectedCount();
